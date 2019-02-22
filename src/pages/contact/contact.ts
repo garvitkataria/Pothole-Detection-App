9b785ad10345bd70011cd2b5d30b25612b6e24e0
garvitkataria <garvit.k16@iiits.in>
2019-02-23 02:18:57 +0530

    Feature Map

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,7 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+	@ViewChild("map") mapElement;
+  map: any;
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ngOnInit(){
+    this.initMap();
+  }
+
+initMap(){
+
+    let coords = new google.maps.LatLng(13.5566268,80.0249917);
+    let mapOptions: google.maps.MapOptions = {
+      center: coords,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
+
+    let marker: google.maps.Marker = new google.maps.Marker({
+      map: this.map,
+      position: coords
+    })
 
   }
 
